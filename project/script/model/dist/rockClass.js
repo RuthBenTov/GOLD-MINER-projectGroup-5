d5afab1004726bf29f051de15bb2c97c9a7b1648
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-07-21 01:51:34 +0300

    ruth

--- project/script/model/dist/rockClass.js
+++ project/script/model/dist/rockClass.js
@@ -18,23 +18,55 @@ var Rocks = /** @class */ (function () {
     Rocks.prototype.renderRock = function () {
         var game = document.querySelector(".container__footer");
         if (game) {
-            game.innerHTML += "\n      <img class=\"rockElem\" \n          id=\"" + this.id + "\" src=\"" + this.imgSrc + "\" alt=\"goldImg\" \n          style=\"width:" + this.width + "px; top: " + this.position.yPos + "px; \"left: " + this.position.xPos + "px\"\">";
+            game.innerHTML += "\n      <img class=\"rockElem\" \n          id=\"" + this.id + "\" src=\"" + this.imgSrc + "\" alt=\"goldImg\" \n          style=\"width:" + this.width + "px; top: " + this.position.yPos + "%; left: " + this.position.xPos + "%\">";
         }
     };
     return Rocks;
 }());
-var rockMapLevel1 = [
-    new Rocks("stone", 7, 30, { xPos: 200, yPos: 100 }),
-    new Rocks("stone", 11, 30, { xPos: 100, yPos: 100 }),
-    new Rocks("gold", 9, 90, { xPos: 100, yPos: 100 }),
-    new Rocks("stone", 3, 30, { xPos: 100, yPos: 100 }),
-    new Rocks("bag", 7, 40, { xPos: 100, yPos: 100 }),
-    new Rocks("stone", 14, 30, { xPos: 100, yPos: 100 }),
-    new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
-    new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
-    new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+var rockMap1Level1 = [
+    new Rocks("stone", 7, 30, { xPos: 50, yPos: 50 }),
+    new Rocks("stone", 11, 30, { xPos: 60, yPos: 70 }),
+    new Rocks("stone", 9, 30, { xPos: 40, yPos: 20 }),
+    new Rocks("stone", 3, 30, { xPos: 30, yPos: 30 }),
+    new Rocks("stone", 14, 30, { xPos: 80, yPos: 20 }),
+    new Rocks("stone", 7, 30, { xPos: 20, yPos: 70 }),
+    new Rocks("bag", 7, 20, { xPos: 70, yPos: 50 }),
+    new Rocks("gold", 600, 90, { xPos: 30, yPos: 55 }),
+    new Rocks("gold", 650, 90, { xPos: 5, yPos: 70 }),
+    new Rocks("gold", 300, 70, { xPos: 70, yPos: 70 }),
+    new Rocks("gold", 90, 30, { xPos: 10, yPos: 29 }),
+    new Rocks("gold", 90, 30, { xPos: 12, yPos: 10 }),
 ];
-rockMapLevel1.forEach(function (elem) { return elem.renderRock(); });
+var rockMap2Level1 = [
+    new Rocks("stone", 7, 30, { xPos: 50, yPos: 50 }),
+    new Rocks("stone", 11, 30, { xPos: 60, yPos: 70 }),
+    new Rocks("stone", 9, 30, { xPos: 40, yPos: 20 }),
+    new Rocks("stone", 3, 30, { xPos: 30, yPos: 30 }),
+    new Rocks("stone", 14, 30, { xPos: 80, yPos: 20 }),
+    new Rocks("stone", 7, 30, { xPos: 20, yPos: 70 }),
+    new Rocks("bag", 7, 20, { xPos: 70, yPos: 50 }),
+    new Rocks("gold", 600, 90, { xPos: 30, yPos: 55 }),
+    new Rocks("gold", 650, 90, { xPos: 5, yPos: 70 }),
+    new Rocks("gold", 300, 70, { xPos: 70, yPos: 70 }),
+    new Rocks("gold", 90, 30, { xPos: 10, yPos: 29 }),
+    new Rocks("gold", 90, 30, { xPos: 12, yPos: 10 }),
+];
+;
+var rockMap3Level1 = [
+    new Rocks("stone", 7, 30, { xPos: 50, yPos: 50 }),
+    new Rocks("stone", 11, 30, { xPos: 60, yPos: 70 }),
+    new Rocks("stone", 9, 30, { xPos: 40, yPos: 20 }),
+    new Rocks("stone", 3, 30, { xPos: 30, yPos: 30 }),
+    new Rocks("stone", 14, 30, { xPos: 80, yPos: 20 }),
+    new Rocks("stone", 7, 30, { xPos: 20, yPos: 70 }),
+    new Rocks("bag", 7, 20, { xPos: 70, yPos: 50 }),
+    new Rocks("gold", 600, 90, { xPos: 30, yPos: 55 }),
+    new Rocks("gold", 650, 90, { xPos: 5, yPos: 70 }),
+    new Rocks("gold", 300, 70, { xPos: 70, yPos: 70 }),
+    new Rocks("gold", 90, 30, { xPos: 10, yPos: 29 }),
+    new Rocks("gold", 90, 30, { xPos: 12, yPos: 10 }),
+];
+rockMap1Level1.forEach(function (elem) { return elem.renderRock(); });
 function createUniqId() {
     return String(Date.now().toString(32) + Math.random().toString(16)).replace(/\./g, "");
 }
