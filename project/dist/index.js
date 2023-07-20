5f88f07bc6346cbb93ed470f73d9f6ab597a2739
moria-avraham <128312303+moria-avraham@users.noreply.github.com>
2023-07-20 19:23:51 +0300

    .

---
+++ project/dist/index.js
@@ -0,0 +1,9 @@
+var gold = /** @class */ (function () {
+    function gold(id, goldImg, width, position) {
+        this.id = id;
+        this.goldImg = goldImg;
+        this.width = width;
+        this.position = position;
+    }
+    return gold;
+}());
---
+++ project/index.ts
@@ -0,0 +1,3 @@
+class gold {
+    constructor(public id: number, public goldImg: string, public width: number, public position: string) { }
+}
