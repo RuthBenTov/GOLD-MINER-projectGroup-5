0d87dce75c18a76360bae80a08e746b4671fec80
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-07-21 01:28:31 +0300

    ruth

--- project/script/model/dist/rockClass.js
+++ project/script/model/dist/rockClass.js
@@ -1,17 +1,40 @@
 var Rocks = /** @class */ (function () {
-    function Rocks(id, type, //gold, stone, bag
-    goldImg, value, //how much it worth
-    width, position) {
+    function Rocks(type, //gold, stone, bag
+    value, //how much it worth
+    width, position, id) {
         if (id === void 0) { id = createUniqId(); }
-        this.id = id;
         this.type = type;
-        this.goldImg = goldImg;
         this.value = value;
         this.width = width;
         this.position = position;
+        this.id = id;
+        if (this.type === "gold")
+            this.imgSrc = "../image/gold.png";
+        if (this.type === "stone")
+            this.imgSrc = "../image/stone.png";
+        if (this.type === "bag")
+            this.imgSrc = "../image/bag.png";
     }
+    Rocks.prototype.renderRock = function () {
+        var game = document.querySelector(".container__footer");
+        if (game) {
+            game.innerHTML += "\n      <img class=\"rockElem\" \n          id=\"" + this.id + "\" src=\"" + this.imgSrc + "\" alt=\"goldImg\" \n          style=\"width:" + this.width + "px; top: " + this.position.yPos + "px; \"left: " + this.position.xPos + "px\"\">";
+        }
+    };
     return Rocks;
 }());
+var rockMapLevel1 = [
+    new Rocks("stone", 7, 30, { xPos: 200, yPos: 100 }),
+    new Rocks("stone", 11, 30, { xPos: 100, yPos: 100 }),
+    new Rocks("gold", 9, 90, { xPos: 100, yPos: 100 }),
+    new Rocks("stone", 3, 30, { xPos: 100, yPos: 100 }),
+    new Rocks("bag", 7, 40, { xPos: 100, yPos: 100 }),
+    new Rocks("stone", 14, 30, { xPos: 100, yPos: 100 }),
+    new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+    new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+    new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+];
+rockMapLevel1.forEach(function (elem) { return elem.renderRock(); });
 function createUniqId() {
     return String(Date.now().toString(32) + Math.random().toString(16)).replace(/\./g, "");
 }
--- project/script/model/rockClass.ts
+++ project/script/model/rockClass.ts
@@ -1,13 +1,41 @@
 class Rocks {
+  public imgSrc: string;
   constructor(
-    public id = createUniqId(),
     public type: string, //gold, stone, bag
-    public goldImg: string,
     public value: number, //how much it worth
     public width: number,
     public position: { xPos: number; yPos: number },
-  ) {}
+    public id = createUniqId(),
+  ) {
+    if (this.type === "gold") this.imgSrc = "../image/gold.png";
+    if (this.type === "stone") this.imgSrc = "../image/stone.png";
+    if (this.type==="bag") this.imgSrc = "../image/bag.png";
+  }
+
+  renderRock() {
+    const game = document.querySelector(".container__footer") as HTMLDivElement;
+    if (game) {
+      game.innerHTML += `
+      <img class="rockElem" 
+          id="${this.id}" src="${this.imgSrc}" alt="goldImg" 
+          style="width:${this.width}px; top: ${this.position.yPos}px; "left: ${this.position.xPos}px"">`;
+    }
+  }
 }
+
+const rockMapLevel1 = [
+  new Rocks("stone", 7, 30, { xPos: 200, yPos: 100 }),
+  new Rocks("stone", 11, 30, { xPos: 100, yPos: 100 }),
+  new Rocks("gold", 9, 90, { xPos: 100, yPos: 100 }),
+  new Rocks("stone", 3, 30, { xPos: 100, yPos: 100 }),
+  new Rocks("bag", 7, 40, { xPos: 100, yPos: 100 }),
+  new Rocks("stone", 14, 30, { xPos: 100, yPos: 100 }),
+  new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+  new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+  new Rocks("stone", 7, 30, { xPos: 100, yPos: 100 }),
+];
+
+rockMapLevel1.forEach((elem) => elem.renderRock());
 
 function createUniqId(): string {
   return String(Date.now().toString(32) + Math.random().toString(16)).replace(
