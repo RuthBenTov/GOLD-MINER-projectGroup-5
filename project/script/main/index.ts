2e0189a60cdf8fcaf36d4330c37f73dadd2e32d0
Shtig7 <128290647+Shtig7@users.noreply.github.com>
2023-07-21 21:51:39 +0300

    1

--- project/script/main/index.ts
+++ project/script/main/index.ts
@@ -1,11 +1,11 @@
-const rope = document.querySelector(".header__rope") as HTMLDivElement;
+ const rope = document.querySelector(".header__rope") as HTMLDivElement;
 // rope.animate({transform: "rotate:180deg"})
 
 document.addEventListener("keydown", handleDown);
 
 function handleDown(ev: any) {
   if (ev.key === "ArrowDown") {
-    rope.style.width = "500px";
+    rope.style.width = "400px";
     rope.classList.add("active");
   }
   else {
