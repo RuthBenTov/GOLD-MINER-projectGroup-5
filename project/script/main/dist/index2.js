9006a1f9b0099dba220fe84953e9669fd9cfc9e1
Ruth_BenTov_developer <112100490+RuthBenTov@users.noreply.github.com>
2023-07-24 21:44:03 +0300

    Update index2.js

--- project/script/main/dist/index2.js
+++ project/script/main/dist/index2.js
@@ -1,24 +1,4 @@
-// setInterval(() => {
-//     const rope = document.querySelector(".header__rope") as HTMLElement;;
-//     const ropeRect = rope.getBoundingClientRect();
-//     checkCollision(ropeRect);
-// }, 10);
-// // בדיקת התנגשות בין האבנים לבין ה-DIV המתארך
-// function checkCollision(ropeRect) {
-//     const rocks = document.querySelectorAll(".rockElem");
-//     rocks.forEach((rock, index) => {
-//         const rockRect = rock.getBoundingClientRect();
-//         if (
-//             ropeRect.left < rockRect.right &&
-//             ropeRect.right > rockRect.left &&
-//             ropeRect.top < rockRect.bottom &&
-//             ropeRect.bottom > rockRect.top
-//         ) {
-//             console.log(index);
-//             // ניתן להוסיף פעולות נוספות שצריך לבצע במידה ויש התנגשות
-//         }
-//     });
-// }
+
 var rope = document.querySelector(".header__rope");
 // setInterval(() => {
 //     const ropeRect = rope.getBoundingClientRect();
