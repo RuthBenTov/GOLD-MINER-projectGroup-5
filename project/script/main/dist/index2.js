08adf7455e009ceb6e287a0ffb3bc8d1b0534ea9
ruttur66 <112100490+RuthBenTov@users.noreply.github.com>
2023-07-24 13:43:34 +0300

    ruth

--- project/script/main/dist/index2.js
+++ project/script/main/dist/index2.js
@@ -1,47 +1,4 @@
-// setInterval(() => {
-//     const rope = document.querySelector(".header__rope") as HTMLElement;;
-//     const ropeRect = rope.getBoundingClientRect();
-//     checkCollision(ropeRect);
-// }, 10);
-// // בדיקת התנגשות בין האבנים לבין ה-DIV המתארך
-// function checkCollision(ropeRect) {
-//     const rocks = document.querySelectorAll(".rockElem");
-//     rocks.forEach((rock, index) => {
-//         const rockRect = rock.getBoundingClientRect();
-//         if (
-//             ropeRect.left < rockRect.right &&
-//             ropeRect.right > rockRect.left &&
-//             ropeRect.top < rockRect.bottom &&
-//             ropeRect.bottom > rockRect.top
-//         ) {
-//             console.log(index);
-//             // ניתן להוסיף פעולות נוספות שצריך לבצע במידה ויש התנגשות
-//         }
-//     });
-// }
 var rope = document.querySelector(".header__rope");
-// setInterval(() => {
-//     const ropeRect = rope.getBoundingClientRect();
-//     checkCollision(ropeRect);
-// }, 10);
-// // בדיקת התנגשות בין האבנים לבין ה-DIV המתארך
-// function checkCollision(ropeRect) {
-//     const rocks = document.querySelectorAll(".rockElem");
-//     for (let i = 0; i < rocks.length; i++) {
-//         const rocklocation = rocks[i].getBoundingClientRect();
-//         const item = rocks[i]
-//         if (
-//             ropeRect.left < rocklocation.right &&
-//             ropeRect.right > rocklocation.left &&
-//             ropeRect.top < rocklocation.bottom &&
-//             ropeRect.bottom > rocklocation.top
-//         ) {
-//             console.log(item);
-//             LiftingTheRocks(item)
-//             // setInterval(LiftingTheRocks, 1000);
-//         }
-//     }
-// }
 function LiftingTheRocks(item) {
     var element = document.querySelector(".rockElem");
     console.log(item);
@@ -82,28 +39,6 @@ function checkCollision(ropeBottom, ropeLeft, ropeRect) {
         }
     }
 }
-// function updateTargetPosition() {
-//   try {
-//     const sourceRect = bart.getBoundingClientRect();
-//     const targetRect = shoot.getBoundingClientRect();
-//     shoot.style.left =
-//       parseFloat(getComputedStyle(shoot).left) + offsetX + "px";
-//     shoot.style.top = parseFloat(getComputedStyle(shoot).top) + offsetY + "px";
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-// function checkCollisionOnRopeMovement(ropeBottom, ropeLeft) {
-//     const rope = document.querySelector(".header__rope") as HTMLElement;
-//     // const ropeRect = rope.getBoundingClientRect();
-//     const ropeStyle = getComputedStyle(rope);
-//     const ropeTop = ropeRect.offsetTop;
-//     const ropeLeft = ropeRect.offsetLeft;
-//     checkCollision(ropeRect, ropeTop, ropeLeft);
-//     // console.log(ropeTop, ropeLeft)
-// }
-// הפעלת הפונקציה כל פרק זמן מסוים
-// setInterval(checkCollisionOnRopeMovement, 10);
 setInterval(function () {
     var ropeBottom = rope.getBoundingClientRect().bottom;
     var ropeLeft = rope.getBoundingClientRect().left;
--- project/script/main/index2.ts
+++ project/script/main/index2.ts
@@ -1,56 +1,5 @@
-// setInterval(() => {
-//     const rope = document.querySelector(".header__rope") as HTMLElement;;
-//     const ropeRect = rope.getBoundingClientRect();
-//     checkCollision(ropeRect);
-// }, 10);
-
-// // בדיקת התנגשות בין האבנים לבין ה-DIV המתארך
-// function checkCollision(ropeRect) {
-//     const rocks = document.querySelectorAll(".rockElem");
-
-//     rocks.forEach((rock, index) => {
-//         const rockRect = rock.getBoundingClientRect();
-
-//         if (
-//             ropeRect.left < rockRect.right &&
-//             ropeRect.right > rockRect.left &&
-//             ropeRect.top < rockRect.bottom &&
-//             ropeRect.bottom > rockRect.top
-//         ) {
-//             console.log(index);
-
-//             // ניתן להוסיף פעולות נוספות שצריך לבצע במידה ויש התנגשות
-//         }
-//     });
-// }
 
 const rope = document.querySelector(".header__rope") as HTMLElement;
-// setInterval(() => {
-
-//     const ropeRect = rope.getBoundingClientRect();
-//     checkCollision(ropeRect);
-// }, 10);
-
-// // בדיקת התנגשות בין האבנים לבין ה-DIV המתארך
-// function checkCollision(ropeRect) {
-//     const rocks = document.querySelectorAll(".rockElem");
-//     for (let i = 0; i < rocks.length; i++) {
-//         const rocklocation = rocks[i].getBoundingClientRect();
-//         const item = rocks[i]
-
-//         if (
-//             ropeRect.left < rocklocation.right &&
-//             ropeRect.right > rocklocation.left &&
-//             ropeRect.top < rocklocation.bottom &&
-//             ropeRect.bottom > rocklocation.top
-//         ) {
-//             console.log(item);
-//             LiftingTheRocks(item)
-//             // setInterval(LiftingTheRocks, 1000);
-//         }
-
-//     }
-// }
 
 function LiftingTheRocks(item) {
     const element = document.querySelector(".rockElem") as HTMLElement;
@@ -101,32 +50,6 @@ function checkCollision(ropeBottom, ropeLeft, ropeRect) {
     }
   }
 }
-
-// function updateTargetPosition() {
-//   try {
-//     const sourceRect = bart.getBoundingClientRect();
-//     const targetRect = shoot.getBoundingClientRect();
-
-//     shoot.style.left =
-//       parseFloat(getComputedStyle(shoot).left) + offsetX + "px";
-//     shoot.style.top = parseFloat(getComputedStyle(shoot).top) + offsetY + "px";
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
-
-// function checkCollisionOnRopeMovement(ropeBottom, ropeLeft) {
-//     const rope = document.querySelector(".header__rope") as HTMLElement;
-//     // const ropeRect = rope.getBoundingClientRect();
-//     const ropeStyle = getComputedStyle(rope);
-//     const ropeTop = ropeRect.offsetTop;
-//     const ropeLeft = ropeRect.offsetLeft;
-//     checkCollision(ropeRect, ropeTop, ropeLeft);
-//     // console.log(ropeTop, ropeLeft)
-// }
-
-// הפעלת הפונקציה כל פרק זמן מסוים
-// setInterval(checkCollisionOnRopeMovement, 10);
 setInterval(() => {
   const ropeBottom = rope.getBoundingClientRect().bottom;
   const ropeLeft = rope.getBoundingClientRect().left;
