c4257bcf682892a134376ee8a706e02b3557db16
danieltovbin <111869103+danieltovbin@users.noreply.github.com>
2023-07-29 15:59:02 +0300

    homePage,signUpDone

--- project/script/main/dist/signUp.js
+++ project/script/main/dist/signUp.js
@@ -1,6 +1,4 @@
 // the page of playerClass.ts must be open 
-// Get playes from localStorage
-var players = [];
 document.addEventListener("DOMContentLoaded", function () {
     var signUpLink = document.querySelector("#signUpLink");
     if (!signUpLink)
@@ -8,33 +6,38 @@ document.addEventListener("DOMContentLoaded", function () {
     signUpLink.addEventListener("click", handleSignUpClick);
 });
 function handleSignUpClick(ev) {
-    ev.preventDefault();
-    var userNameInput = document.querySelector("#userName");
-    var emailInput = document.querySelector("#email");
-    var passwordInput = document.querySelector("#password");
-    if (!userNameInput.value || !emailInput.value || !passwordInput.value) {
-        alert("Please fill in all inputs first.");
-    }
-    else {
-        var nameUser_1 = userNameInput.value;
-        var email_1 = emailInput.value;
-        var password = passwordInput.value;
-        var playerExisting = players.find(function (player) { return player.nameUser === nameUser_1 && player.email === email_1; });
-        if (playerExisting) {
-            // Player already exists 
-            alert("You are already joined us :)");
-            window.location.href = "../view/homePage.html";
+    try {
+        ev.preventDefault();
+        var userNameInput = document.querySelector("#userName");
+        var emailInput = document.querySelector("#email");
+        var passwordInput = document.querySelector("#password");
+        if (!userNameInput.value || !emailInput.value || !passwordInput.value) {
+            alert("Please fill in all inputs first.");
         }
         else {
-            // Player does not exist 
-            players.push(new player(nameUser_1, email_1, password));
-            // save the updated player array to localstorage
-            var playersJson = JSON.stringify(players);
-            localStorage.setItem("players", playersJson);
-            // Show the "We are happy you joined us!" message
-            alert("We are happy you joined us!");
-            window.location.href = "../view/homePage.html";
+            var nameUser_1 = userNameInput.value;
+            var email_1 = emailInput.value;
+            var password = passwordInput.value;
+            var playerExisting = players.find(function (player) { return player.nameUser === nameUser_1 && player.email === email_1; });
+            if (playerExisting) {
+                // Player already exists 
+                alert("You are already joined us :)");
+                window.location.href = "../view/homePage.html";
+            }
+            else {
+                // Player does not exist 
+                players.push(new Player(nameUser_1, email_1, password));
+                // save the updated player array to localstorage
+                var playersJson = JSON.stringify(players);
+                localStorage.setItem("players", playersJson);
+                // Show the "We are happy you joined us!" message
+                alert("We are happy you joined us!");
+                window.location.href = "../view/homePage.html";
+            }
         }
+    }
+    catch (error) {
+        console.error(error);
     }
 }
 var playerString = localStorage.getItem("players");
@@ -42,6 +45,6 @@ if (playerString) {
     // Convert string to array
     var playerArray = JSON.parse(playerString);
     playerArray.forEach(function (playerData) {
-        players.push(new player(playerData.nameUser, playerData.email, playerData.password));
+        players.push(new Player(playerData.nameUser, playerData.email, playerData.password));
     });
 }
--- project/script/main/signUp.ts
+++ project/script/main/signUp.ts
@@ -1,9 +1,5 @@
 // the page of playerClass.ts must be open 
 
-// Get playes from localStorage
-const players: player[] = [];
-
-
 document.addEventListener("DOMContentLoaded", ()=>{
   const signUpLink = document.querySelector("#signUpLink") as HTMLAnchorElement;
   if(!signUpLink) throw new Error ("Could not found signUpLink element");
@@ -14,41 +10,45 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
 
   function handleSignUpClick(ev){
-    ev.preventDefault();
-    const userNameInput = document.querySelector("#userName") as HTMLInputElement;
-    const emailInput = document.querySelector("#email") as HTMLInputElement;
-    const passwordInput = document.querySelector("#password") as HTMLInputElement;
-
-    if (!userNameInput.value || !emailInput.value || !passwordInput.value){
-      alert("Please fill in all inputs first.")
-    } else {
-      const nameUser = userNameInput.value;
-      const email = emailInput.value;
-      const password = passwordInput.value;
-      
-
-      const playerExisting = players.find(
-        (player) => player.nameUser === nameUser && player.email === email
-      );
-
-      if(playerExisting) {
-        // Player already exists 
-        alert("You are already joined us :)")
-
-        window.location.href = "../view/homePage.html";
-      }else {
-        // Player does not exist 
-        players.push(new player(nameUser, email, password));
-
-        // save the updated player array to localstorage
-        const playersJson = JSON.stringify(players);
-        localStorage.setItem("players", playersJson);
-
-        // Show the "We are happy you joined us!" message
-        alert("We are happy you joined us!")
-
-        window.location.href = "../view/homePage.html";
+    try {
+      ev.preventDefault();
+      const userNameInput = document.querySelector("#userName") as HTMLInputElement;
+      const emailInput = document.querySelector("#email") as HTMLInputElement;
+      const passwordInput = document.querySelector("#password") as HTMLInputElement;
+  
+      if (!userNameInput.value || !emailInput.value || !passwordInput.value){
+        alert("Please fill in all inputs first.")
+      } else {
+        const nameUser = userNameInput.value;
+        const email = emailInput.value;
+        const password = passwordInput.value;
+        
+  
+        const playerExisting = players.find(
+          (player) => player.nameUser === nameUser && player.email === email
+        );
+  
+        if(playerExisting) {
+          // Player already exists 
+          alert("You are already joined us :)")
+  
+          window.location.href = "../view/homePage.html";
+        }else {
+          // Player does not exist 
+          players.push(new Player(nameUser, email, password));
+  
+          // save the updated player array to localstorage
+          const playersJson = JSON.stringify(players);
+          localStorage.setItem("players", playersJson);
+  
+          // Show the "We are happy you joined us!" message
+          alert("We are happy you joined us!")
+  
+          window.location.href = "../view/homePage.html";
+        }
       }
+    } catch (error) {
+      console.error(error);
     }
     
   }
@@ -59,6 +59,6 @@ document.addEventListener("DOMContentLoaded", ()=>{
     // Convert string to array
     const playerArray = JSON.parse(playerString);
     playerArray.forEach((playerData)=> {
-      players.push(new player(playerData.nameUser, playerData.email, playerData.password));
+      players.push(new Player(playerData.nameUser, playerData.email, playerData.password));
     });
   }
